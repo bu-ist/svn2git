30c67c3d7306962149c9acb41b430abb42bc8687
inderpreet99 <inderpreet99@gmail.com>
2016-08-22 14:55:34 -0400

    Fix tags script

--- inc/svn2git-tags.sh
+++ inc/svn2git-tags.sh
@@ -1,4 +1,3 @@
-
 #! /bin/bash
 #
 # Convert tag branches created during `git svn clone` to annotated Git tags.
@@ -10,19 +9,19 @@
 # NOTE: You should alway review the tag branches to make sure there are no actual
 # changes committed to them that would be lost by force deleting.
 #
- 
+
 for tag_branch in `git branch | grep "tags/"`; do
- 
+
 	# Extract numeric tag from branch name
 	tag=${tag_branch:5}
- 
+
 	# Find the parent commit of commit captured in the tag branch
 	commit=`git merge-base $tag_branch master`
- 
+
 	# Create an annotated tag
 	echo "Creating tag $tag for branch $tag_branch ($commit)."
 	git tag -am "Release $tag" $tag $commit
-	
+
 	# Remove the old tag branch
 	echo "Tag branch $tag_branch removed."
 	git branch -D $tag_branch
